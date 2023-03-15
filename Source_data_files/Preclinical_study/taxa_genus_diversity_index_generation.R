3208f77e39340d9e279418e0b8931c40176f9b88
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2023-03-15 15:23:21 +0000

    Create taxa_genus_diversity_index_generation.R

---
+++ Source_data_files/Preclinical_study/taxa_genus_diversity_index_generation.R
@@ -0,0 +1,24 @@
+library(vegan)
+setwd("~/")
+
+X <- read.csv("taxa_raw_count.csv",row.names=1)
+head(X)
+row.names(X)
+Y <- t(X)
+
+########## compute the diversity analysis
+
+invsimp <- diversity(Y, "invsimpson")
+shannon <- diversity(Y,"shannon")
+
+
+############ print the diversity index
+
+rbind(invsimp)
+rbind(shannon)
+
+# show all indices in column
+cbind(invsimp,shannon,alpha)
+
+Z <- cbind(invsimp,shannon)
+Z
